3196e15bb1e4e29fb618319c8b33a1ced0c5ae12
artialpha <arturszafraniak6@gmail.com>
2022-03-31 21:27:14 +0200

    Testing if i can step forward and step back in a quicksort

--- Window/Algs/QuickSortDraw.py
+++ Window/Algs/QuickSortDraw.py
@@ -20,6 +20,11 @@ class QuickSortDraw(QuickSort):
     def __getitem__(self, index):
         return self.steps[index]
 
+    def __str__(self):
+        return f'{self.data}\nright: i{self.right_index}, v{self.data[self.right_index]}' \
+               f'\nleft: i{self.left_index}, v{self.data[self.left_index]}' \
+               f'\npivot: i{self.pivot_index}, v{self.data[self.pivot_index]}'
+
     def sort_data(self, left_index, right_index, data):
         if len(data) == 1:
             return data
@@ -33,34 +38,79 @@ class QuickSortDraw(QuickSort):
 
     def partition(self, left_index, right_index, data):
         pivot_index = right_index
-        self.steps.append((self.place_pivot_indexes, (left_index, right_index, pivot_index)))
+        self.steps.append((self.place_pivot_indexes,
+                           (
+                               (left_index, right_index, pivot_index),
+                               (self.left_index, self.right_index, self.pivot_index)
+                           )))
 
         while left_index < right_index:
             if data[left_index] >= data[pivot_index]:
                 if data[right_index] < data[pivot_index]:
                     # swap values
                     data[left_index], data[right_index] = data[right_index], data[left_index]
-                    self.steps.append(self.swap_left_right)
+                    self.steps.append((self.swap_index, ('left-right', )))
                 else:
                     right_index -= 1
-                    self.steps.append(self.decrease_right)
+                    self.steps.append((self.move_index, ('right', -1)))
             else:
                 left_index += 1
-                self.steps.append(self.increase_left)
+                self.steps.append((self.move_index, ('left', 1)))
 
         data[pivot_index], data[right_index] = data[right_index], data[pivot_index]
-        self.steps.append(self.swap_right_pivot)
+        self.steps.append((self.swap_index, ('right-pivot', )))
+        self.left_index, self.right_index, self.pivot_index = left_index, right_index, pivot_index
         # right_index is the place of a new pivot
         return right_index
 
     def perform_steps(self):
         print("i do steps")
-        for step in self.steps:
-            # print(step)
-            if callable(step):
-                step()
-            else:
-                step[0](*step[1])
+        i = 0
+
+        while i < len(self.steps):
+            self.steps[i][0](*self.steps[i][1])
+            i += 1
+
+        '''
+        print(self)
+        while i < len(self.steps):
+
+            x = input()
+            if x == 'next':
+                print(self.steps[i])
+
+                self.steps[i][0](*self.steps[i][1])
+                print('after step', i)
+                print(self)
+                i += 1
+            elif x == 'prev' and i != 0:
+                i -= 1
+            print("nothing")
+        '''
+
+    def step_back(self, index):
+        # print('in step back')
+        method = self.steps[index][0]
+        method_arguments = self.steps[index][1]
+        if method_arguments[0] == 'left' or method_arguments[0] == 'right':
+            index, value = method_arguments
+            value *= -1
+            method(*(index, value))
+        elif method_arguments[0] == 'left-right' or method_arguments[0] == 'right-pivot':
+            method(*method_arguments)
+        else:
+            print(method_arguments[1])
+            method(method_arguments[1])
+
+    def swap_index(self, *args):
+        if args[0] == 'left-right':
+            self.data[self.left_index], self.data[self.right_index] \
+                = self.data[self.right_index], self.data[self.left_index]
+        elif args[0] == 'right-pivot':
+            self.data[self.pivot_index], self.data[self.right_index] \
+                = self.data[self.right_index], self.data[self.pivot_index]
+        else:
+            print('i did not do anything', args)
 
     def swap_left_right(self):
         self.data[self.left_index], self.data[self.right_index] \
@@ -70,13 +120,14 @@ class QuickSortDraw(QuickSort):
         self.data[self.pivot_index], self.data[self.right_index] \
             = self.data[self.right_index], self.data[self.pivot_index]
 
-    def decrease_right(self):
-        self.right_index -= 1
-
-    def increase_left(self):
-        self.left_index += 1
+    def move_index(self, *args):
+        if args[0] == 'left':
+            self.left_index += args[1]
+        else:
+            self.right_index += args[1]
 
     def place_pivot_indexes(self, *args):
-        self.left_index = args[0]
-        self.right_index = args[1]
-        self.pivot_index = args[2]
+        new_places = args[0]
+        self.left_index = new_places[0]
+        self.right_index = new_places[1]
+        self.pivot_index = new_places[2]
--- Window/Algs/test_QuickSortDraw.py
+++ Window/Algs/test_QuickSortDraw.py
@@ -5,14 +5,140 @@ import random
 
 first_test = (
     [5, 1, 2, 4, 3],
-    [1, 3, 5, 4, 2],
-    [9, 6, 3, 2, 1, 7, 5, 4, 8]
+    #[1, 3, 5, 4, 2],
+    #[9, 6, 3, 2, 1, 7, 5, 4, 8]
 )
 
 data_test = [random.sample(range(100), 10) for y in range(20)]
 
 
 class TestQuickSortDraw(TestCase):
+
+    def test_step_back_1(self):
+        """
+        list of steps for test: [5, 1, 2, 4, 3]
+        ((0, 4, 4), (0, 0, 0)) counter 0
+        ('right', -1) counter 1             r3
+        ('right', -1) counter 2             r2
+        ('left-right',) counter 3           [2, 1, 5, 4, 3]
+        ('left', 1) counter 4               l1
+        ('left', 1) counter 5               l2
+        ('right-pivot',) counter 6          [2, 1, 3, 4, 5]
+        ((0, 1, 1), (2, 2, 4)) counter 7    l0 r1 p1
+        ('right', -1) counter 8             r0
+        ('right-pivot',) counter 9          [1, 2, 3, 4, 5]
+        ((3, 4, 4), (0, 0, 1)) counter 10   l3 r4 p4
+        ('left', 1) counter 11              l4
+        ('right-pivot',) counter 12
+
+        :return:
+        """
+
+        draw_list = []
+
+        for unsorted in first_test:
+            draw = QuickSortDraw(copy.copy(unsorted))
+            draw.sort_data(0, len(unsorted)-1, unsorted)
+            draw_list.append(draw)
+
+        for draw in draw_list:
+            for count, step in enumerate(draw.steps):
+                print(step[1], f'counter {count}')
+
+        draw_to_test = draw_list[0]
+        step = 0
+
+        # 0 step = place pivot, right and left
+        draw_to_test.steps[step][0](*draw_to_test.steps[step][1])
+        self.assertEqual(draw_to_test.right_index, 4)
+
+        # 1 step = right--
+        step += 1
+        draw_to_test.steps[step][0](*draw_to_test.steps[step][1])
+        self.assertEqual(draw_to_test.right_index, 3)
+
+        # 2 step = right --
+        step += 1
+        draw_to_test.steps[step][0](*draw_to_test.steps[step][1])
+        self.assertEqual(draw_to_test.right_index, 2)
+
+        # 3 step = swap left nad right value
+        # before swap:  left = 5, right = 2
+        # after:        right = 2, left = 5
+        step += 1
+        self.assertEqual(draw_to_test.data[draw_to_test.right_index], 2)
+        self.assertEqual(draw_to_test.data[draw_to_test.left_index], 5)
+        draw_to_test.steps[step][0](*draw_to_test.steps[step][1])
+        self.assertEqual(draw_to_test.data[draw_to_test.right_index], 5)
+        self.assertEqual(draw_to_test.data[draw_to_test.left_index], 2)
+        print(draw_to_test, '')
+
+        # Now i go back
+        # performing a "step back" should swap back values at left and right index
+        # I don't change the value of the step because I want to undo step at this index
+        draw_to_test.step_back(step)
+        self.assertEqual(draw_to_test.data[draw_to_test.right_index], 2)
+        self.assertEqual(draw_to_test.data[draw_to_test.left_index], 5)
+
+        # Undo 2 step - it means that right index should be increased by one
+        step -= 1
+        self.assertEqual(draw_to_test.right_index, 2)
+        draw_to_test.step_back(step)
+        self.assertEqual(draw_to_test.right_index, 3)
+
+        # Undo 1 step - it means that right index should be increased by one
+        step -= 1
+        self.assertEqual(draw_to_test.right_index, 3)
+        draw_to_test.step_back(step)
+        self.assertEqual(draw_to_test.right_index, 4)
+
+        # undo 0 step - place indexes back to the starting position
+        self.assertEqual(draw_to_test.left_index, 0)
+        self.assertEqual(draw_to_test.right_index, 4)
+        self.assertEqual(draw_to_test.pivot_index, 4)
+        step -= 1
+        draw_to_test.step_back(step)
+        self.assertEqual(draw_to_test.left_index, 0)
+        self.assertEqual(draw_to_test.right_index, 0)
+        self.assertEqual(draw_to_test.pivot_index, 0)
+
+        print(step, 'step 0')
+        # move to 4th step
+        for s in range(5):
+            draw_to_test.steps[s][0](*draw_to_test.steps[s][1])
+            print(s, 's')
+            step = s
+        self.assertEqual(draw_to_test.left_index, 1)
+
+        # 5 step
+        step += 1
+        draw_to_test.steps[step][0](*draw_to_test.steps[step][1])
+        self.assertEqual(draw_to_test.left_index, 2)
+
+        # 6 step swap values of right and pivot
+        step += 1
+        self.assertEqual(draw_to_test.data[draw_to_test.right_index], 5)
+        self.assertEqual(draw_to_test.data[draw_to_test.pivot_index], 3)
+        draw_to_test.steps[step][0](*draw_to_test.steps[step][1])
+        self.assertEqual(draw_to_test.data[draw_to_test.right_index], 3)
+        self.assertEqual(draw_to_test.data[draw_to_test.pivot_index], 5)
+
+        # 7 step new place for indexes
+        step += 1
+        self.assertEqual(draw_to_test.left_index, 2)
+        self.assertEqual(draw_to_test.right_index, 2)
+        self.assertEqual(draw_to_test.pivot_index, 4)
+        draw_to_test.steps[step][0](*draw_to_test.steps[step][1])
+        self.assertEqual(draw_to_test.left_index, 0)
+        self.assertEqual(draw_to_test.right_index, 1)
+        self.assertEqual(draw_to_test.pivot_index, 1)
+
+        # 8 step
+        step += 1
+        self.assertEqual(draw_to_test.right_index, 1)
+        draw_to_test.steps[step][0](*draw_to_test.steps[step][1])
+        self.assertEqual(draw_to_test.right_index, 0)
+
 
     def test_first_test(self):
         draw_list = []
@@ -25,7 +151,7 @@ class TestQuickSortDraw(TestCase):
         for draw in draw_list:
             print(draw.data, 'before')
             draw.perform_steps()
-            print(draw.data, 'after')
+            print(draw.data, 'after\n')
 
         for draw in draw_list:
             self.assertTrue(all(draw.data[i] <= draw.data[i+1] for i in range(len(draw.data) - 1)))
